299cf38406592e87cd57f6b803925516544bec63
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-09-07 13:59:04 +0300

    start adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { PhotoCamera } from "@material-ui/icons";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <PhotoCamera />
+                    <Typography variant="h6">
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </>
     );
 }
 
