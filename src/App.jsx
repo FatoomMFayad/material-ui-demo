4a1a437e21631d9783d241dada589c1131459ce5
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-09-07 14:48:07 +0300

    add typography and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
 import { PhotoCamera } from "@material-ui/icons";
 
 const App = () => {
@@ -14,6 +14,32 @@ const App = () => {
                     </Typography>
                 </Toolbar>
             </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Photo Album
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            This is a photo album and I am trying to make this sentence as long as possible to see how it is look in the screen
+                        </Typography>
+                        <div>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    <Button variant='contained' color="primary">
+                                        See my photos
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant='outlined' color="primary">
+                                        Secondary action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+            </main>
         </>
     );
 }
