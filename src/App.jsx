04b07543134e4a61caccfc1cd4ef6d6012a3b0a7
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-09-08 14:29:12 +0300

    adding footer and its style

--- src/App.jsx
+++ src/App.jsx
@@ -66,6 +66,12 @@ const App = () => {
                     </Grid>
                 </Container>
             </main>
+            <footer className={classes.footer}>
+                <Typography variant="h6" align="center" gutterBottom>Footer</Typography>
+                <Typography variant="subtitle1" align="center" color="textSecondary" >
+                    Something here to give the footer a purpose!
+                </Typography>
+            </footer>
         </>
     );
 }
