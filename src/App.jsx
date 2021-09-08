8037ab3fffc377030a53252c1498e295ff09d79f
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-09-08 14:23:31 +0300

    responsive grid items

--- src/App.jsx
+++ src/App.jsx
@@ -44,8 +44,8 @@ const App = () => {
                 </div>
                 <Container className={classes.cardGrid} maxWidth="md">
                     <Grid container spacing={4}>
-                        {cards.map(() => (
-                            <Grid item>
+                        {cards.map((card) => (
+                            <Grid item key={card} xs={12} sm={6} md={4}>
                             <Card className={classes.card}>
                                 <CardMedia className={classes.cardMedia} image="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80" title="Image title" />
                                 <CardContent className={classes.cardContent}>
